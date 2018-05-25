820daee0f815a305d3314ce16433c745a55b4267
kirchner <kirchner@posteo.de>
2018-05-25 22:06:29 +0200

    switch to new elm version

--- examples/shell.nix
+++ examples/shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 {
   elm = pkgs.stdenv.mkDerivation rec {
-    name = "elm-0.19-alpha";
+    name = "elm-0.19-alpha-2";
     src = pkgs.fetchurl {
       url = https://44a95588fe4cc47efd96-ec3c2a753a12d2be9f23ba16873acc23.ssl.cf2.rackcdn.com/linux-64.tar.gz;
-      sha256 = "182f1zzhawqay7k5gsbn5ig1ysc50x9zq69ww1kd68fmwdam01yq";
+      sha256 = "0gmsjb2cnccfib6149n3c65pdkii2qcfdjihkg513vjr6nzpz43y";
     };
     libPath = pkgs.stdenv.lib.makeLibraryPath [
         pkgs.zlib pkgs.ncurses5 pkgs.gmp pkgs.libffi
